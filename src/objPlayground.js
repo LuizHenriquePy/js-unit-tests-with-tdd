b01c3e75161772d3bb673f780bacb20908995383
Luiz Henrique <luizhenriquemendes123123@gmail.com>
2022-06-21 19:47:36 -0300

    implementa functions calculator e arrayGenerator

--- src/objPlayground.js
+++ src/objPlayground.js
@@ -1,3 +1,4 @@
+/* eslint-disable radix */
 /*
   Implemente a função calculator que recebe dois números inteiros como parâmetro e retorna um objeto com as seguintes chaves:
     - sum;
@@ -34,8 +35,16 @@
   arrayGenerator('entries', { sum: 3, mult: 2, div: 0, sub: -1 }) // [ [ 'sum', 3 ], [ 'mult', 2 ], [ 'div', 0 ], [ 'sub', -1 ] ]
 */
 
-const calculator = (number1, number2) => {};
+const calculator = (number1, number2) => ({ 
+  sum: number1 + number2, 
+  mult: number1 * number2, 
+  div: parseInt(number1 / number2),
+  sub: number1 - number2 });
 
-const arrayGenerator = (type, object) => {};
+const arrayGenerator = (type, object) => {
+  if (type === 'keys') { return Object.keys(object); }
+  if (type === 'values') { return Object.values(object); }
+  if (type === 'entries') { return Object.entries(object); }
+};
 
-module.exports = { calculator, arrayGenerator };
+module.exports = { calculator, arrayGenerator }; 
