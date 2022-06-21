c09f4cde5117eabcca2e051b8ad51d493955b2c5
Luiz Henrique <luizhenriquemendes123123@gmail.com>
2022-06-21 10:54:11 -0300

    Comentário adicionado no testa da function numbers

--- tests/numbers.spec.js
+++ tests/numbers.spec.js
@@ -14,6 +14,7 @@ const numbers = require('../src/numbers');
 
 */
 
+// Códigos expects implementado por Luiz Henrique
 describe('2 - Implemente os casos de teste para a função `numbers`', () => {
   it('Verifica se a função `numbers` retorna `true` quando o array contém apenas números e falso caso contrário', () => {
     expect(numbers([1, 2, 3, 4, 5])).toBeTruthy();
