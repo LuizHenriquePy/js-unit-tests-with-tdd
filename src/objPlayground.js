fb08969b5280db8cb4d340542938ff2b79644fd3
Luiz Henrique <luizhenriquemendes123123@gmail.com>
2022-06-21 20:11:06 -0300

    conserta erro de linter na function calculator

--- src/objPlayground.js
+++ src/objPlayground.js
@@ -38,7 +38,7 @@
 const calculator = (number1, number2) => ({ 
   sum: number1 + number2, 
   mult: number1 * number2, 
-  div: parseInt(number1 / number2),
+  div: parseInt(number1 / number2, 10),
   sub: number1 - number2 });
 
 const arrayGenerator = (type, object) => {
